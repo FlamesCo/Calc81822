c2d03991456a7c0a0aef3828a1df424b6eaf1d50
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-08-18 11:33:34 -0700

    Request to upload 8.18.22 #0 FST

---
+++ Calc.java
@@ -0,0 +1,23 @@
+public class Calc {
+    // Write A Calculator in java
+          public static void main(String[] args) {
+        System.out.println("1 + 1 = " + (1 + 1));
+        System.out.println("1 - 1 = " + (1 - 1));
+        System.out.println("1 * 1 = " + (1 * 1));
+        System.out.println("1 / 1 = " + (1 / 1));
+        System.out.println("1 % 1 = " + (1 % 1));
+        System.out.println("1 + 1 + 1 = " + (1 + 1 + 1));
+        System.out.println("1 - 1 - 1 = " + (1 - 1 - 1));
+        System.out.println("1 * 1 * 1 = " + (1 * 1 * 1));
+        System.out.println("1 / 1 / 1 = " + (1 / 1 / 1));
+        System.out.println("1 % 1 % 1 = " + (1 % 1 % 1));
+        System.out.println("1 + 1 + 1 + 1 = " + (1 + 1 + 1 + 1));
+        System.out.println("1 - 1 - 1 - 1 = " + (1 - 1 - 1 - 1));
+        System.out.println("1 * 1 * 1 * 1 = " + (1 * 1 * 1 * 1));
+        System.out.println("1 / 1 / 1 / 1 = " + (1 / 1 / 1 / 1));
+        System.out.println("1 % 1 % 1 % 1 = " + (1 % 1 % 1 % 1));
+        System.out.println("1 + 1 + 1 + 1 + 1 = " + (1 + 1 + 1 + 1 + 1));
+        System.out.println("1 - 1 - 1 - 1 - 1 = " + (1 - 1 - 1 - 1 - 1));
+        System.out.println("1 * 1 * 1 * 1 * 1 = " + (1 * 1 * 1 * 1 * 1));
+        System.out.println("1 / 1 / 1 / 1 / 1 = " + (1 / 1 / 1 / 1 / 1));
+          }}
